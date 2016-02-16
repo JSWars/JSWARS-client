e64430b1df6d15898527008484b5732efd534e91
marcx <djwedo@gmail.com>
2016-02-17 00:34:44 +0100

    analytics

--- app/js/main.js
+++ app/js/main.js
@@ -112,12 +112,20 @@ require([
 
 ], function (app) {
 
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-1018881-6', 'auto');
+	ga('send', 'pageview');
+
 
 	app.config(['$logProvider', '$locationProvider', function ($logProvider, $locationProvider) {
 		$logProvider.debugEnabled(true);
 	}]);
 
-	app.run(['$rootScope', '$http', 'ngProgressFactory', function ($rootScope, $http, ngProgressFactory) {
+	app.run(['$rootScope', '$http', 'ngProgressFactory', '$location', '$window', function ($rootScope, $http, ngProgressFactory, $location, $window) {
 		var progressbar = ngProgressFactory.createInstance();
 
 		$rootScope.isLoading = function () {
@@ -131,5 +139,15 @@ require([
 				progressbar.complete();
 			}
 		});
+
+		$rootScope
+			.$on('$stateChangeSuccess',
+			function (event) {
+
+				if (!$window.ga)
+					return;
+
+				$window.ga('send', 'pageview', {page: $location.path()});
+			});
 	}])
 });
