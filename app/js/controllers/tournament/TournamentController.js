572abf6a7b86a2bc4e0030c032f1b9254dbef3bd
marcx <djwedo@gmail.com>
2016-02-22 12:37:14 +0100

    Torneos OK

--- app/js/controllers/tournament/TournamentController.js
+++ app/js/controllers/tournament/TournamentController.js
@@ -4,26 +4,40 @@ define([
 		'services/TournamentService',
 		'directives/TournamentJoinDialogDrtv'
 	], function (Controllers) {
-		Controllers.controller("TournamentController", ['$scope', '$state', '$stateParams', 'State', 'TournamentService', function ($scope, $state, $stateParams, State, TournamentService) {
+		Controllers.controller("TournamentController", ['$scope', '$state', '$timeout', '$stateParams', 'State', 'TournamentService', function ($scope, $state, $timeout, $stateParams, State, TournamentService) {
 
-			TournamentService.get({id: $stateParams.id})
-				.then(function (tournament) {
-					State.setState({
-						title: "Tournament: " + tournament.name
-					});
 
-					$scope.tournament = tournament;
+			var getTournament = function () {
+				TournamentService.get({id: $stateParams.id})
+					.then(function (tournament) {
+						State.setState({
+							title: "Tournament: " + tournament.name
+						});
 
-					$scope.tournament.roundDetails = [];
+						$scope.tournament = tournament;
 
-					for (var i = $scope.tournament.rounds; i > 0; i--) {
-						$scope.tournament.roundDetails.push({
-							round: i,
-							battles: Math.pow(2, i - 1),
-							agents: Math.pow(2, i)
-						})
-					}
-				})
+						$scope.tournament.roundDetails = [];
+
+						for (var i = $scope.tournament.rounds; i > 0; i--) {
+							$scope.tournament.roundDetails.push({
+								round: i,
+								battles: Math.pow(2, i - 1),
+								agents: Math.pow(2, i)
+							})
+						}
+
+						if (tournament.status == 'RUNNING') {
+							$timeout(getTournament, 5000);
+						}
+
+					})
+			};
+
+			getTournament();
+
+			$scope.refresh = function () {
+				$state.reload();
+			};
 		}]);
 	}
 );
